9a0170841090831acd752c16b2405b776dc177b7
Rizwan Khan <rizwi23.rk@gmail.com>
2021-01-04 00:34:00 +0530

    added 3d effect to clock

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ function clock() {
   const minute = minutes * 6;
   const second = seconds * 6;
 
-  document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
-  document.querySelector(".minute").style.transform = `rotate(${minute}deg)`;
+  const hourHand = document.querySelector(".hour");
+  const minuteHand = document.querySelector(".minute");
   const secondHand = document.querySelector(".second");
+
+  hourHand.style.transform = `rotate(${hour}deg)`;
+  minuteHand.style.transform = `rotate(${minute}deg)`;
   second == 0
     ? secondHand.classList.add("hide-transition")
     : secondHand.classList.remove("hide-transition");
   secondHand.style.transform = `rotate(${second}deg)`;
+
+  // dynamic shadow
+  let hourOffsetSign = hour > 135 && hour < 315 ? "-" : "";
+  let minuteOffsetSign = minute > 135 && minute < 315 ? "-" : "";
+  let secondOffsetSign = second > 135 && second < 315 ? "-" : "";
+
+  hourHand.style.boxShadow = `${hourOffsetSign}6px ${hourOffsetSign}6px 6px #b8b9be`;
+  minuteHand.style.boxShadow = `${minuteOffsetSign}6px ${minuteOffsetSign}6px 6px #b8b9be`;
+  secondHand.style.boxShadow = `${secondOffsetSign}6px ${secondOffsetSign}6px 6px #b8b9be`;
 }
 
 setInterval(clock, 1000);
