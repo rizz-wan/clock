39fa6e0f54904637478103a9f79557e3e6f1a543
Rizwan Khan <rizwi23.rk@gmail.com>
2021-04-11 13:41:07 +0530

    Improvised, can be used as proper screen saver

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Clock
-This is a neumorphic analog clock.
+A neumorphic clock setup.
 ## Running project locally
 - Clone Repo
 - Open [index.html](./index.html) in the browser
+## Using code/snippet
+- Make sure there is a link that traces back to this repo
+## Feedback
+- If you find any bugs or issues please report them on the [Issue Tracker](https://github.com/rizz-wan/clock/issues).
+> If you would like to contribute to this project please consider [forking this repo](https://github.com/rizz-wan/clock/fork), making your changes and then creating a new [Pull Request](https://github.com/rizz-wan/clock/pulls) back to the main code repository.
--- index.js
+++ index.js
@@ -30,6 +30,47 @@ function clock() {
   hourHand.style.boxShadow = `${hourOffsetSign}6px ${hourOffsetSign}6px 6px #b8b9be`;
   minuteHand.style.boxShadow = `${minuteOffsetSign}6px ${minuteOffsetSign}6px 6px #b8b9be`;
   secondHand.style.boxShadow = `${secondOffsetSign}6px ${secondOffsetSign}6px 6px #b8b9be`;
+  
+  //V2
+  const weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  document.getElementById("clock-digi").innerHTML =
+    weekday[date.getDay()] +
+    ", " +
+    makeDouble(date.getDate()) +
+    "/" +
+    makeDouble(date.getMonth()) +
+    " - " +
+    makeDouble(hours) +
+    ":" +
+    makeDouble(minutes);
+
+  document.getElementById("clock-sec").innerHTML = makeDouble(seconds);
 }
 
 setInterval(clock, 1000);
+
+function goFullscreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    /* Safari */
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    /* IE11 */
+    document.documentElement.msRequestFullscreen();
+  }
+}
+
+function makeDouble(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
